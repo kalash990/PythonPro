d5feb68c167fc6b14603676103be8d62332e6761
kalash001 <kalash7151970@yahoo.com>
2023-03-20 22:01:19 +0530

    Night

---
+++ PythonProject/Var.py
@@ -0,0 +1,27 @@
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+#
+# ####################################
+# #Write your code below this line 👇
+#
+# c = a
+# a = b
+# b = c
+#
+# #Write your code above this line 👆
+# ####################################
+#
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+#from Tools.demo.beer import n
+
+a = 'Welcome to the Band Name Generator.'
+print(a)
+b = input('What is the name of the city you grew up in? \n')
+print(b)
+c = input('What is your pet\'s name? \n')
+print(c)
+print(f'Your Band Name is {b} {c}...')
